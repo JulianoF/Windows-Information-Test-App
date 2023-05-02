121017ed4a049012ff926684529e47ce87bfa1c5
JulianoF <juliano1@live.ca>
2023-05-02 12:22:15 -0400

    GUI Updates

--- WindowsStatus.Designer.cs
+++ WindowsStatus.Designer.cs
@@ -28,134 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.runButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.pcNameCheckBox = new System.Windows.Forms.CheckBox();
+            this.osCheckBox = new System.Windows.Forms.CheckBox();
+            this.cpuTypeCheckBox = new System.Windows.Forms.CheckBox();
+            this.cpuSpeedCheckBox = new System.Windows.Forms.CheckBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.checkBox5 = new System.Windows.Forms.CheckBox();
-            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.ramCheckBox = new System.Windows.Forms.CheckBox();
+            this.cDriveCheckBox = new System.Windows.Forms.CheckBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // runButton
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(33, 340);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(360, 47);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Run Basic ";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.runButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.runButton.Location = new System.Drawing.Point(33, 329);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(388, 47);
+            this.runButton.TabIndex = 0;
+            this.runButton.Text = "Run Basic ";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Courier Std", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(29, 29);
+            this.label1.Location = new System.Drawing.Point(29, 33);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(296, 21);
+            this.label1.Size = new System.Drawing.Size(351, 21);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Basic System Status Report";
+            this.label1.Text = "Basic System Information Report";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // checkBox1
+            // pcNameCheckBox
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBox1.Location = new System.Drawing.Point(33, 107);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(84, 20);
-            this.checkBox1.TabIndex = 2;
-            this.checkBox1.Text = "PC Name";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.pcNameCheckBox.AutoSize = true;
+            this.pcNameCheckBox.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pcNameCheckBox.Location = new System.Drawing.Point(33, 107);
+            this.pcNameCheckBox.Name = "pcNameCheckBox";
+            this.pcNameCheckBox.Size = new System.Drawing.Size(84, 20);
+            this.pcNameCheckBox.TabIndex = 2;
+            this.pcNameCheckBox.Text = "PC Name";
+            this.pcNameCheckBox.UseVisualStyleBackColor = true;
+            this.pcNameCheckBox.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
-            // checkBox2
+            // osCheckBox
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBox2.Location = new System.Drawing.Point(33, 142);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(98, 20);
-            this.checkBox2.TabIndex = 3;
-            this.checkBox2.Text = "OS Version";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.osCheckBox.AutoSize = true;
+            this.osCheckBox.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.osCheckBox.Location = new System.Drawing.Point(33, 142);
+            this.osCheckBox.Name = "osCheckBox";
+            this.osCheckBox.Size = new System.Drawing.Size(98, 20);
+            this.osCheckBox.TabIndex = 3;
+            this.osCheckBox.Text = "OS Version";
+            this.osCheckBox.UseVisualStyleBackColor = true;
             // 
-            // checkBox3
+            // cpuTypeCheckBox
             // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBox3.Location = new System.Drawing.Point(33, 179);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(90, 20);
-            this.checkBox3.TabIndex = 4;
-            this.checkBox3.Text = "CPU Type";
-            this.checkBox3.UseVisualStyleBackColor = true;
+            this.cpuTypeCheckBox.AutoSize = true;
+            this.cpuTypeCheckBox.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cpuTypeCheckBox.Location = new System.Drawing.Point(33, 179);
+            this.cpuTypeCheckBox.Name = "cpuTypeCheckBox";
+            this.cpuTypeCheckBox.Size = new System.Drawing.Size(90, 20);
+            this.cpuTypeCheckBox.TabIndex = 4;
+            this.cpuTypeCheckBox.Text = "CPU Type";
+            this.cpuTypeCheckBox.UseVisualStyleBackColor = true;
             // 
-            // checkBox4
+            // cpuSpeedCheckBox
             // 
-            this.checkBox4.AutoSize = true;
-            this.checkBox4.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBox4.Location = new System.Drawing.Point(33, 215);
-            this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(98, 20);
-            this.checkBox4.TabIndex = 5;
-            this.checkBox4.Text = "CPU Speed";
-            this.checkBox4.UseVisualStyleBackColor = true;
-            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            this.cpuSpeedCheckBox.AutoSize = true;
+            this.cpuSpeedCheckBox.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cpuSpeedCheckBox.Location = new System.Drawing.Point(33, 215);
+            this.cpuSpeedCheckBox.Name = "cpuSpeedCheckBox";
+            this.cpuSpeedCheckBox.Size = new System.Drawing.Size(98, 20);
+            this.cpuSpeedCheckBox.TabIndex = 5;
+            this.cpuSpeedCheckBox.Text = "CPU Speed";
+            this.cpuSpeedCheckBox.UseVisualStyleBackColor = true;
+            this.cpuSpeedCheckBox.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(30, 70);
+            this.label2.Location = new System.Drawing.Point(30, 74);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(363, 16);
+            this.label2.Size = new System.Drawing.Size(391, 16);
             this.label2.TabIndex = 6;
-            this.label2.Text = "Select The Information you want the Status Report to Display";
+            this.label2.Text = "Select The Information you want the Information Report to Display";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // checkBox5
+            // ramCheckBox
             // 
-            this.checkBox5.AutoSize = true;
-            this.checkBox5.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBox5.Location = new System.Drawing.Point(33, 253);
-            this.checkBox5.Name = "checkBox5";
-            this.checkBox5.Size = new System.Drawing.Size(115, 20);
-            this.checkBox5.TabIndex = 7;
-            this.checkBox5.Text = "Installed RAM";
-            this.checkBox5.UseVisualStyleBackColor = true;
+            this.ramCheckBox.AutoSize = true;
+            this.ramCheckBox.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ramCheckBox.Location = new System.Drawing.Point(33, 253);
+            this.ramCheckBox.Name = "ramCheckBox";
+            this.ramCheckBox.Size = new System.Drawing.Size(115, 20);
+            this.ramCheckBox.TabIndex = 7;
+            this.ramCheckBox.Text = "Installed RAM";
+            this.ramCheckBox.UseVisualStyleBackColor = true;
             // 
-            // checkBox6
+            // cDriveCheckBox
             // 
-            this.checkBox6.AutoSize = true;
-            this.checkBox6.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBox6.Location = new System.Drawing.Point(33, 289);
-            this.checkBox6.Name = "checkBox6";
-            this.checkBox6.Size = new System.Drawing.Size(135, 20);
-            this.checkBox6.TabIndex = 8;
-            this.checkBox6.Text = "Size of C:\\ Drive";
-            this.checkBox6.UseVisualStyleBackColor = true;
+            this.cDriveCheckBox.AutoSize = true;
+            this.cDriveCheckBox.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cDriveCheckBox.Location = new System.Drawing.Point(33, 289);
+            this.cDriveCheckBox.Name = "cDriveCheckBox";
+            this.cDriveCheckBox.Size = new System.Drawing.Size(135, 20);
+            this.cDriveCheckBox.TabIndex = 8;
+            this.cDriveCheckBox.Text = "Size of C:\\ Drive";
+            this.cDriveCheckBox.UseVisualStyleBackColor = true;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.checkBox6);
-            this.groupBox1.Controls.Add(this.checkBox5);
+            this.groupBox1.Controls.Add(this.cDriveCheckBox);
+            this.groupBox1.Controls.Add(this.ramCheckBox);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.checkBox4);
-            this.groupBox1.Controls.Add(this.checkBox3);
-            this.groupBox1.Controls.Add(this.checkBox2);
-            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.cpuSpeedCheckBox);
+            this.groupBox1.Controls.Add(this.cpuTypeCheckBox);
+            this.groupBox1.Controls.Add(this.osCheckBox);
+            this.groupBox1.Controls.Add(this.pcNameCheckBox);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.runButton);
             this.groupBox1.Location = new System.Drawing.Point(1, 1);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(449, 420);
+            this.groupBox1.Size = new System.Drawing.Size(449, 398);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
@@ -164,7 +164,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(450, 421);
+            this.ClientSize = new System.Drawing.Size(450, 405);
             this.Controls.Add(this.groupBox1);
             this.Name = "MainWindow";
             this.Text = "Windows Status (Alpha)";
@@ -176,15 +176,15 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button runButton;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.CheckBox checkBox3;
-        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox pcNameCheckBox;
+        private System.Windows.Forms.CheckBox osCheckBox;
+        private System.Windows.Forms.CheckBox cpuTypeCheckBox;
+        private System.Windows.Forms.CheckBox cpuSpeedCheckBox;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.CheckBox checkBox5;
-        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox ramCheckBox;
+        private System.Windows.Forms.CheckBox cDriveCheckBox;
         private System.Windows.Forms.GroupBox groupBox1;
     }
 }

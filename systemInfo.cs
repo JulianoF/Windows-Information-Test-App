9a2611a4e83e07fa5c2bf978109403c810636551
JulianoF <juliano1@live.ca>
2023-05-03 13:22:42 -0400

    added systemInfo Class to facilitate pulling enviroment variables + other changes and progress

---
+++ systemInfo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsStatusTestAPP
+{
+    public class systemInfo
+    {
+        bool showName;
+        bool showVersion;
+        bool showType;
+        bool showSpeed;
+        bool showRam;
+        bool showDrive;
+        public systemInfo()
+        {
+            showName = false;
+            showVersion = false;
+            showType = false;
+            showSpeed = false;
+            showRam = false;
+            showDrive = false;
+        }
+
+        public void setName(bool value)
+        {
+            if (value)
+            {
+                showName = true;
+            }
+            else
+            {
+                showName= false;
+            }
+        }
+
+        public void getSysInfo()
+        {
+            if (showName)
+            {
+                Console.WriteLine(Environment.MachineName.ToString());
+            }
+            
+        }
+    }
+}
